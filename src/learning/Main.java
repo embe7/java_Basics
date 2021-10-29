af6905527caeb5e68d5af9f677a47deb1f6d188b
ACER <hendry45xtc@gmail.com>
2021-10-29 21:56:30 +0700

    Tipe Data Number

--- src/learning/Main.java
+++ src/learning/Main.java
@@ -4,7 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Word");
-	System.out.println("my name is suhendri");
+
+        /* tipe data number
+
+           byte
+           min = -28 | max = 127 */
+
+           byte iniByte = 100;
+
+           /*s hort
+           min = 32,768 | max = 32,767 */
+
+           short iniShort = 1000;
+
+           /* int
+           min = -2,147,438,648 | max = 2,147,483,648 */
+
+           int iniInteger = 1000000;
+
+           /* long
+            min = -9,223,372,036,854,775,808 | max = 9,223,372,036,854,775,807 */
+
+           long iniLong = 1000000000;
+
+           /* float
+           min = 3.4e-038 | max = 3.4e+038 */
+
+           float iniFloat = 10.10F;
+
+           /* double
+           min = 1.7e-308 max =1.7e+308 */
+
+           double iniDouble = 20.20;
+
     }
 }
