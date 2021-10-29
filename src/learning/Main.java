a305ce2f7c6dc4e835d02c1ec5c92acbf08b28f7
ACER <hendry45xtc@gmail.com>
2021-10-29 22:21:39 +0700

    Tipe Data Character

--- src/learning/Main.java
+++ src/learning/Main.java
@@ -5,17 +5,25 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        //Konversi Tipe Data Number
+        //Tipe Data Character
 
-        //otomatis
+        char s = 'S';
+        char u = 'U';
+        char h = 'H';
+        char e = 'E';
+        char n = 'N';
+        char d = 'D';
+        char r = 'R';
+        char i = 'I';
 
-        byte iniByte = 10;
-        short iniShort = iniByte;
-        int iniInteger = iniShort;
-
-        //manual
-        long iniLong = 100;
-        byte iniByte2 = (byte) iniLong;
+        System.out.print(s);
+        System.out.print(u);
+        System.out.print(h);
+        System.out.print(e);
+        System.out.print(n);
+        System.out.print(d);
+        System.out.print(r);
+        System.out.print(i);
 
 
 
