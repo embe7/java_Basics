7f311c051fe9594068aa0aef1d3a0746e6b93f9c
ACER <hendry45xtc@gmail.com>
2021-10-29 22:07:49 +0700

    Konversi Tipe Data Number

--- src/learning/Main.java
+++ src/learning/Main.java
@@ -5,37 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        /* tipe data number
+        //Konversi Tipe Data Number
 
-           byte
-           min = -28 | max = 127 */
+        //otomatis
 
-           byte iniByte = 100;
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInteger = iniShort;
 
-           /*s hort
-           min = 32,768 | max = 32,767 */
+        //manual
+        long iniLong = 100;
+        byte iniByte2 = (byte) iniLong;
 
-           short iniShort = 1000;
 
-           /* int
-           min = -2,147,438,648 | max = 2,147,483,648 */
-
-           int iniInteger = 1000000;
-
-           /* long
-            min = -9,223,372,036,854,775,808 | max = 9,223,372,036,854,775,807 */
-
-           long iniLong = 1000000000;
-
-           /* float
-           min = 3.4e-038 | max = 3.4e+038 */
-
-           float iniFloat = 10.10F;
-
-           /* double
-           min = 1.7e-308 max =1.7e+308 */
-
-           double iniDouble = 20.20;
 
     }
 }
