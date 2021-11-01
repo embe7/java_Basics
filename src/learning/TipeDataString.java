405227923087e12be005d00ea148b54d8200ff8f
ACER <hendry45xtc@gmail.com>
2021-11-01 20:54:02 +0700

    Tipe Data String

---
+++ src/learning/TipeDataString.java
@@ -0,0 +1,13 @@
+package learning;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+
+        String name = "Suhendri";
+        String myBio = " Adalah Seorang Pelajar";
+
+        String data = name + myBio;
+
+        System.out.println(data);
+    }
+}
