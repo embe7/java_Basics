c8e77f98be72b9240cdbd7c930ebd5afb771ab44
ACER <hendry45xtc@gmail.com>
2021-11-01 21:45:13 +0700

    variabel

---
+++ src/learning/variabel.java
@@ -0,0 +1,21 @@
+package learning;
+
+public class variabel {
+    public static void main(String[] args) {
+
+        String name;
+        name = "suhendri";
+
+        System.out.println(name);
+
+        int age = 23;
+        final String addres;
+        final var universitas = "Universitas Budidarma Medan";
+        addres = "Takengon, Indonesia";
+
+        System.out.println(age);
+        System.out.println(addres);
+        System.out.println(universitas);
+
+    }
+}
