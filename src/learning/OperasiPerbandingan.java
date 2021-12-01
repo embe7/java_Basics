0e4aba2095634fac87df7efa719fac96b2cbe41e
ACER <hendry45xtc@gmail.com>
2021-12-02 00:31:28 +0700

    Operasi Perbandingan

---
+++ src/learning/OperasiPerbandingan.java
@@ -0,0 +1,25 @@
+package learning;
+
+public class OperasiPerbandingan {
+
+    /*
+        - Operasi perbandingan adalah operasi yang digunakan untuk membandingkan dua buah data
+        - Operasi perbandingan adalah operasi yang menghasilkan nilai boolean (benar atau salah)
+        - Jika hasil operasinya adalah benar, maka nilainya adalah true
+        - jika hasil operasinya adalah salah, maka nilainya adalah false
+     */
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 100;
+
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+    }
+}
