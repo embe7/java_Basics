92a82f75ebd1aa3fdeface8bbb0df3f4cc2b839d
ACER <hendry45xtc@gmail.com>
2021-12-04 21:23:02 +0700

    For Loop

---
+++ src/learning/ForLoop.java
@@ -0,0 +1,16 @@
+package learning;
+
+public class ForLoop {
+
+    /*
+        - For adalah salah satu kata kunci untuk melakukan perulangan
+        - Blok kode yang terdapat didalam for akan selalu diulangi selama kondisi for terpenuhi
+     */
+
+    public static void main(String[] args) {
+
+        for (var counter = 1; counter <= 10; counter++){
+            System.out.println("Perulangan ke-" + counter);
+        }
+    }
+}
