1768092d0902779640578dee767c96e58235b9a2
ACER <hendry45xtc@gmail.com>
2021-12-04 21:01:38 +0700

    Ternary Operator

---
+++ src/learning/TernaryOperator.java
@@ -0,0 +1,18 @@
+package learning;
+
+public class TernaryOperator {
+
+    /*
+        - Ternary operator adalah operator sedehana dari if statement
+        - Ternary operator terdiri dari kondisi yang dievaluasi, jika menghasilkan true maka nilai pertama diambil,
+          jika false maka nilai kedua yang diambil
+     */
+
+    public static void main(String[] args) {
+
+        var nilai = 75;
+        String ucapan = nilai >= 75? "Selamat anda lulus" : "Anda Tidak Lulus";
+
+        System.out.println(ucapan);
+    }
+}
