271b14fad9e2037bb84205503f798e3ad6122934
ACER <hendry45xtc@gmail.com>
2021-12-15 13:13:27 +0700

    Latihan ForLoop

--- src/learning/ForLoop.java
+++ src/learning/ForLoop.java
@@ -9,8 +9,8 @@ public class ForLoop {
 
     public static void main(String[] args) {
 
-        for (var counter = 1; counter <= 10; counter++){
-            System.out.println("Perulangan ke-" + counter);
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Perulangan ke-" + i);
         }
     }
 }
