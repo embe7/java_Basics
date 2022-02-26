e14dc681a224014ba3f93eac0646d5c8b61fb6f2
embe7 <embe7>
2022-02-26 14:08:45 +0700

    Latihan ForLoop

---
+++ src/latihan/LatihanLooping/ForLoop.java
@@ -0,0 +1,10 @@
+package latihan.LatihanLooping;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++){
+            System.out.println("Perulangan ke-" + i);
+        }
+    }
+}
